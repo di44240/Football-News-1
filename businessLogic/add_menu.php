87276ea5b6dcd2383ac8656ae33211ca5a05515a
Dita Ibrahimi <di44240@ubt-uni.net>
2021-03-17 14:04:45 +0100

    Create add_menu.php

---
+++ businessLogic/add_menu.php
@@ -0,0 +1,80 @@
+<?php
+    require_once '../controllers/MenuController.php';
+
+    $add = new MenuController;
+
+    if(isset($_POST['addMenu-submit'])) {
+        $add->store($_POST);
+    }
+?>
+<!DOCTYPE html>
+ <html lang="en">
+<head>
+    <meta charset = "UTF-8">
+    <title>Football News | Menu</title>
+    <link rel = "stylesheet" href="../views/d/style1.css">
+</head>
+
+<body>
+
+
+    <div class="sidebar">
+        <div class="sidebar-brand">
+        <div class="logo">
+                <a href="index.php"><h3>Football News</h3></a>
+            </div>
+        </div>
+    
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="../views/dashboard1.php"><span class="la-dashboard"></span>
+                    <span>Dashboard</span></a>
+                </li>
+                <li>
+                   <a href="../views/dashboard_create.php"><span class="users"></span>
+                    <span>Users</span></a>
+                </li>
+                <li>
+                    <a href="../views/dashboard_menu.php" class="active"><span class="menu"></span>
+                    <span>Menu</span></a>
+                </li> 
+                <li>
+                    <a href="../views/dashboard_news.php" ><span class="news"></span>
+                    <span>News</span></a>
+                </li> 
+                <li>
+                    <a href="../views/dashboard_contact.php"><span class="feedback"></span>
+                    <span>Feedback</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+        <div class="main-content">
+            <header>
+                <h2>
+                    <label for="">
+                        <span class="las-la-bars"></span>
+                    </label>
+                    
+                </h2>
+
+                <div class="user-wrapper">
+                    <img src="d/wall.jpg" width="40px" height="30px" alt="">
+                <div>
+                    <h4> <a href="dashboard_create.php"><?php echo $_SESSION['name']; ?> </h4>
+                    <small><a href=Account.php>Log out</a></small>
+                </div>
+                </div>
+            </header>
+
+</head>
+<body>
+    <br><br><br><br><br>
+    <form action="" method="post">
+        Type the name of the menu you want to add: <input type="text" placeholder="name..." name="name">
+        <input type="submit" name="addMenu-submit" value="submit"/>
+    </form>
+</body>
+</html>
